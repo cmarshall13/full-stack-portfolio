ea8be213390d62470094fcd2c38409707f3dbbdc
Corinne Marshall <corinnemarshall@Corinnes-MacBook-Air.local>
2022-01-18 13:49:52 -0500

    added a footer and nav with new dependencies

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function Footer() {
+  return (
+    <footer className="flex flex-col justify-center w-3/5 py-4 m-auto text-gray-300">
+          <div className="">
+        
+            © 2022 Corinne Marshall
+            </div>
+
+        </footer>
+  )
+}
