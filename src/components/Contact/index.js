c0d5df444729c16a26130c27b450d43fe63a0934
Corinne Marshall <corinnemarshall@Corinnes-MacBook-Air.local>
2022-01-19 13:48:53 -0500

    adding styling to contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -34,42 +34,75 @@ const Contact = () => {
       console.log('Handle Form', formState);
     }
   };
-
-    return (
-        <div className="row">
-        <h3 className='header'>Contact </h3>
-        <form className="col s12" onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="input-field col s12 m6 l6">
-              <input id="first_name" type="text" className="validate white-text" name='name' onBlur={handleChange}/>
-              <label htmlFor="first_name">Name</label>
+        return (
+          <>
+            <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+              <div className="max-w-md w-full space-y-8">
+                <div>
+                  <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Contact Me</h2>
+                </div>
+                <form className="mt-8 space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
+                  <input type="hidden" name="remember" defaultValue="true" />
+                  <div className="rounded-md shadow-sm -space-y-px">
+                    <div>
+                      <label htmlFor="name" className="sr-only">
+                        Name
+                      </label>
+                      <input
+                        id="name"
+                        name="name"
+                        type="name"
+                        autoComplete="current-name"
+                        required
+                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                        placeholder="Name"
+                        onBlur={handleChange}
+                      />
+                    </div>
+                    <div>
+                      <label htmlFor="email-address" className="sr-only">
+                        Email address
+                      </label>
+                      <input
+                        id="email-address"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        required
+                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                        placeholder="Email address"
+                        onBlur={handleChange}
+                      />
+                    </div>
+                    <div>
+                      <label htmlFor="message" className="sr-only">
+                        Message
+                      </label>
+                      <textarea
+                        id="message"
+                        name="message"
+                        rows={3}
+                        required
+                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                        placeholder="Your Message:"
+                        defaultValue={''}
+                        onBlur={handleChange}
+                      />
+                    </div>
+                  </div>
+      
+                  <div>
+                    <button
+                      type="submit"
+                      className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                      Send
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="input-field col s12 m6 l6">
-              <input id="email" type="email" className="validate white-text" name='email' onBlur={handleChange}/>
-              <label htmlFor="email">Email</label>
-            </div>
-          </div>
-          <div className="row">
-            <div className="input-field col s12">
-            <textarea name='message' id="textarea1" className="" onBlur={handleChange}></textarea>
-            <label htmlFor="textarea1">Message</label>
-            </div>
-          </div>
-          {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
-          </div>
-          )}
-          <div className='row'>
-            <button className="btn waves-effect waves-light black-text" type="submit" name="action">Submit
-            </button>
-          </div>
-          
-        </form>
-      </div>
-    )
-}
-
-export default Contact
+          </>
+        )
+      }
+export default Contact;
