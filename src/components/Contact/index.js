b5d6fa6c11528fa67fb4907c150b0845b9596552
Corinne Marshall <corinnemarshall@Corinnes-MacBook-Air.local>
2022-01-20 13:33:00 -0500

    added styling to contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -36,10 +36,10 @@ const Contact = () => {
   };
         return (
           <>
-            <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-              <div className="max-w-md w-full space-y-8">
+            <div className="h-full w-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+              <div className="h-full w-full space-y-8">
                 <div>
-                  <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Contact Me</h2>
+                  <h2 className="mt-6 text-center text-3xl font-serif text-black">Contact Me</h2>
                 </div>
                 <form className="mt-8 space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
                   <input type="hidden" name="remember" defaultValue="true" />
@@ -54,7 +54,7 @@ const Contact = () => {
                         type="name"
                         autoComplete="current-name"
                         required
-                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm"
                         placeholder="Name"
                         onBlur={handleChange}
                       />
@@ -69,7 +69,7 @@ const Contact = () => {
                         type="email"
                         autoComplete="email"
                         required
-                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm"
                         placeholder="Email address"
                         onBlur={handleChange}
                       />
@@ -83,7 +83,7 @@ const Contact = () => {
                         name="message"
                         rows={3}
                         required
-                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm"
                         placeholder="Your Message:"
                         defaultValue={''}
                         onBlur={handleChange}
@@ -94,7 +94,7 @@ const Contact = () => {
                   <div>
                     <button
                       type="submit"
-                      className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-pink-900 hover:bg-pink-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
                     >
                       Send
                     </button>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col justify-center w-3/5 py-4 m-auto text-gray-300">
+    <footer className="flex flex-wrap justify-center w-full py-4 m-auto text-black">
           <div className="">
         
             © 2022 Corinne Marshall
