457a82c6da214b7aa372cc164a2aad81f494e1f8
Corinne Marshall <corinnemarshall@Corinnes-MacBook-Air.local>
2022-01-21 12:30:43 -0500

    made changes to nav on mobile

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -18,14 +18,13 @@ export default function Navigation({ active, setActive }) {
 
   return (
     <Disclosure as="nav" className="bg-pink-800 font-serif">
-      {({ open }) => (
+      {({open}) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-black hover:text-white hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
@@ -33,6 +32,7 @@ export default function Navigation({ active, setActive }) {
                   )}
                 </Disclosure.Button>
               </div>
+
               <div className="flex-1 flex justify-around items-stretch ">
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
@@ -56,18 +56,17 @@ export default function Navigation({ active, setActive }) {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
-              {categories.map((categories) => (
+              {categories.map((category) => (
                 <Disclosure.Button
-                  key={categories.name}
-                  as="a"
-                  href={categories.href}
-                  className={classNames(
-                    categories.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block px-3 py-2 rounded-md text-base font-medium'
-                  )}
-                  aria-current={categories.current ? 'page' : undefined}
-                >
-                  {categories.name}
+                      className={`" rounded-full  px-4 text-xl lg:px-8 xl:text-2xl  hover:text-white " 
+                      ${currentCategory === category ? " rounded-full  text-black" : "text-white"} `} 
+                      key={category}>
+                      <a href="#" 
+                      className="uppercase" 
+                      onClick={() => { setActive(category); setCurrentCategory(category); }}
+                      >
+                      {category}
+                      </a>
                 </Disclosure.Button>
               ))}
             </div>
